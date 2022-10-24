403b01891812b25509bab0d790e6b262010cb8bb
1211lys <hpppower111@gmail.com>
2022-10-24 12:07:18 +0900

    wip

---
+++ demo/src/App.vue
@@ -0,0 +1,167 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>VSkeletonLoader Demo</h1>
+      <div>
+        <div id="textInput">
+          <input
+            type="text"
+            v-model="textInput"
+            v-on:keyup.enter="submitValue"
+          />
+          <p>Value: {{ textInput }}</p>
+        </div>
+      </div>
+      <div class="samples">
+        <VSkeletonLoader :content="{ submitData }" />
+        <h3>Case #1</h3>
+        <VSkeletonLoader :content="type1" />
+        <h3>Case #2</h3>
+        <VSkeletonLoader :content="type2" />
+        <h3>Case #3</h3>
+        <VSkeletonLoader :content="type3" />
+        <h3>Case #4</h3>
+        <VSkeletonLoader :content="type4" />
+        <h3>Case #5</h3>
+        <VSkeletonLoader :content="type5" />
+        <h3>Case #6</h3>
+        <VSkeletonLoader :content="type6" />
+        <h3>Case #7</h3>
+        <VSkeletonLoader :content="type7" />
+        <h3>Case #8</h3>
+        <VSkeletonLoader :content="type8" />
+        <h3>Case #9</h3>
+        <VSkeletonLoader :content="type9" />
+        <h3>Case #10</h3>
+        <VSkeletonLoader :content="type10" />
+        <h3>Case #11</h3>
+        <VSkeletonLoader :content="type11" />
+        <h3>Case #12</h3>
+        <VSkeletonLoader :content="type12" />
+        <h3>Case #13</h3>
+        <VSkeletonLoader :content="type13" />
+        <h3>Case #14</h3>
+        <VSkeletonLoader :content="type14" />
+        <h3>Case #15</h3>
+        <VSkeletonLoader :content="type15" />
+        <h3>Case #16</h3>
+        <VSkeletonLoader :content="type16" />
+        <h3>Case #17</h3>
+        <VSkeletonLoader :content="type17" />
+        <h3>Case #18</h3>
+        <VSkeletonLoader :content="type18" />
+        <h3>Case #19</h3>
+        <VSkeletonLoader :content="type19" />
+        <h3>Case #20</h3>
+        <VSkeletonLoader :content="type20" />
+        <h3>Case #21</h3>
+        <VSkeletonLoader :content="type21" />
+        <h3>Case #22</h3>
+        <VSkeletonLoader :content="type22" />
+        <h3>Case #23</h3>
+        <VSkeletonLoader :content="type23" />
+        <h3>Case #24</h3>
+        <VSkeletonLoader :content="type24" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/components/VSkeletonLoader";
+import VSkeletonLoader from "./components/VSkeletonLoader.vue";
+export default {
+  name: "App",
+  components: {
+    VSkeletonLoader,
+  },
+  data() {
+    return {
+      type1: [["box", "text"]],
+      type2: [["box", "text:2"]],
+      type3: [["box", "text:3", "text:2"]],
+
+      type4: [["circle", "text"]],
+      type5: [["circle", "text:2"]],
+      type6: [["circle", "text:3"]],
+
+      type7: [["box", "title + text"]],
+      type8: [["box", "title + text:2"]],
+      type9: [["box", "title + text:3"]],
+
+      type10: [["circle", "title + text"]],
+      type11: [["circle", "title + text:2"]],
+      type12: [["circle", "title + text:3"]],
+
+      type13: [["box", "title + text"], ["text"]],
+      type14: [["box", "title + text:2"], ["text:2"]],
+      type15: [["box", "title + text:3"], ["text:3"]],
+
+      type16: [["circle", "title + text"], ["text"]],
+      type17: [["circle", "title + text:2"], ["text:2"]],
+      type18: [["circle", "title + text:3"], ["text:3"]],
+
+      type19: [["box", "title + text"], ["text"], ["text"]],
+      type20: [["box", "title + text:2"], ["text:2"], ["text:2"]],
+      type21: [["box", "title + text:3"], ["text:3"], ["text:3"]],
+
+      type22: [["circle", "title + text"], ["text"], ["text"]],
+      type23: [["circle", "title + text:2"], ["text:2"], ["text:2"]],
+      type24: [["circle", "title + text:3"], ["text:3"], ["text:3"]],
+      textInput: "",
+      submitData: [],
+    };
+  },
+  methods: {
+    handler(e) {
+      console.log(e);
+    },
+    // convertArrStrToArr(arrStr) {
+    //   return Array.of(arrStr.replace(/'/g, '"')).JSON.Par;
+    // },
+
+    convertArrStrToArr(inputStr) {
+      return inputStr
+        .replace(/"/g, "")
+        .replace(/'/g, "")
+        .replace(/\[/g, "")
+        .replace(/], /g, "-")
+        .replace(/]/g, "")
+        .split("-")
+        .map((s) => s.split(",").map((v) => v.trim()))
+        .filter((v) => typeof v === "string");
+    },
+    submitValue() {
+      // console.log(this.convertArrStrToArr());
+      // 빈문자열 확인
+      // 정상적인 배열문장열인지 ("[]", "['ㅈㅇㅈ', 'ㅈㅇㅈㅇ']", "['ㅈㅇㅈ', 'ㅈㅇㅈㅇ', 'ㅇㅈㅇㅈㅇ']")
+
+      // submitData
+      // console.log(this.textInput.replace(/'/, /,/g, '"'));
+      const result = this.convertArrStrToArr("['circle', 'text:3']");
+      console.log(result);
+      try {
+        this.submitData = result;
+        console.log(this.submitData);
+        this.textInput = "";
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
+};
+</script>
+<style>
+#app {
+  width: 800px;
+  margin: 0 auto;
+}
+.container {
+  margin: 20px;
+}
+
+.input {
+  width: 200px;
+  height: 200px;
+}
+</style>
---
+++ demo/src/components/VSkeletonLoader.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="v-skeleton-loader">
+    <div
+      class="row"
+      v-for="(row, rowIndex) in content"
+      :key="rowIndex"
+      v-bind:class="rowIndex"
+    >
+      <span
+        class="col"
+        v-for="(element, elementIndex) in row"
+        :key="elementIndex"
+      >
+        <template v-if="element.includes('circle') || element.includes('box')">
+          <span class="element" :class="element">
+            {{ element }}
+          </span>
+        </template>
+        <!-- <template v-if="element.includes('box')">
+          <span class="element" :class="element">
+            {{ element }}
+          </span>
+        </template> -->
+        <template v-if="element.includes('title')">
+          <span class="element title">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element === 'text'">
+          <span class="element text">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element === 'title + text'">
+          <span class="element text">
+            {{ element }}
+          </span>
+        </template>
+        <template v-if="element.includes('text:')">
+          <span
+            class="element text"
+            :class="element"
+            v-for="(e, i) in parseInt(element.split(':')[1], 10)"
+            :key="i"
+          >
+            {{ element }}
+          </span>
+        </template>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VSkeletonLoader",
+  props: {
+    content: {
+      type: Array,
+    },
+  },
+  //  포털 , 이중배열, 인풋 그려지게
+};
+</script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  margin: -5px;
+}
+.row:nth-child(2) {
+  margin-top: 13px;
+}
+.col {
+  margin: 0 5px;
+}
+.col:last-child {
+  flex: 1 0 auto;
+}
+.element {
+  display: block;
+  position: relative;
+  width: 100%;
+  margin: 5px 0;
+  background-color: #dddbdd;
+  border-radius: 5px;
+  text-indent: -9999px;
+  overflow: hidden;
+}
+.element:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  transform: translateX(-100%);
+  background: linear-gradient(
+    90deg,
+    transparent,
+    rgba(255, 255, 255, 0.5),
+    transparent
+  );
+  animation: shimmer 1.5s infinite;
+}
+@keyframes shimmer {
+  100% {
+    transform: translateX(100%);
+  }
+}
+.element.box {
+  width: 70px;
+  height: 0;
+  padding-bottom: 100%;
+}
+.element.circle {
+  width: 70px;
+  height: 0;
+  padding-bottom: 100%;
+  border-radius: 50%;
+}
+.element.title {
+  height: 20px;
+}
+.element.title:first-child {
+  width: 30%;
+}
+.element.text {
+  height: 15px;
+}
+.element.text:nth-child(2) {
+  width: 70%;
+}
+.element.text:nth-child(3) {
+  width: 50%;
+}
+.element.text:nth-child(4) {
+  width: 60%;
+}
+</style>
